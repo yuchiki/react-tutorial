40dcdcd7952e8338f6f59f2f48201ad6aacdc4c6
Yuki Imai <yuki08281993@gmail.com>
2019-04-20 21:45:04 +0900

    show 'draw' when the game ended in a draw

--- src/index.js
+++ src/index.js
@@ -99,6 +99,8 @@ class Game extends React.Component {
         let status;
         if (winner) {
             status = "Winner: " + winner;
+        } else if (current.squares.every(x=> x !== null)) {
+            status = "draw";
         } else {
             status = "Next player: " + (this.state.xIsNext ? "X" : "O");
         }
