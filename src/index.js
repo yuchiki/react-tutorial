f51e63c787a270c06bb1e303dbac3a79f3add9db
Yuki Imai <yuki08281993@gmail.com>
2019-04-20 18:51:21 +0900

    refactor render function of Board

--- src/index.js
+++ src/index.js
@@ -21,25 +21,15 @@ class Board extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+        let rows = [];
+        for (let i = 0; i < 3; i++) {
+            let cells = [];
+            for (let j = 0; j < 3; j++) {
+                cells.push(this.renderSquare(i * 3 + j));
+            }
+            rows.push(<div className="board-row">{cells}</div>)
+        }
+        return <div>{rows}</div>;
     }
 }
 
